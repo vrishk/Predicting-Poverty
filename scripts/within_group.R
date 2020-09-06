7d529171ec8461a72cf6f60da4ba6292f3b9c56c
vrishk <krishnavrishab@gmail.com>
2020-09-06 17:37:22 +0530

    feat: process and store all features

--- scripts/within_group.R
+++ scripts/within_group.R
@@ -16,7 +16,7 @@ df <- df[, !(names(df) %in% c(
     "Enhanced_Vegetation_Index_2015", "BUILT_Population_2014", 
     "All_Population_Density_2015", "All_Population_Density_2015",
     "All_Population_Count_2015"
-    ))]
+))]
 
 cat(str(df))
 
@@ -25,25 +25,24 @@ cat(str(df))
 
 
 # Climatic and Environmental Variables
-env <- df[c("Aridity", "Enhanced_Vegetation_Index_2010", 
-     "Potential_Evapotranspiration", "Global_Human_Footprint",
-     "Rainfall_2010"
-     )]
+env <- df[c("id", "Aridity", "Enhanced_Vegetation_Index_2010", 
+            "Potential_Evapotranspiration", "Global_Human_Footprint",
+            "Rainfall_2010"
+)]
 
 colnames(env)[colnames(env) == 'Enhanced_Vegetation_Index_2010'] <- 'EVI'
 colnames(env)[colnames(env) == 'Potential_Evapotranspiration'] <- 'Evapotranspiration'
 colnames(env)[colnames(env) == 'Global_Human_Footprint'] <- 'Human Footprint'
 colnames(env)[colnames(env) == 'Rainfall_2010'] <- 'Rainfall'
 
-env_corr <- rcorr(as.matrix(env))
+env_corr <- rcorr(as.matrix(env[, !(names(env) %in% c("id"))]))
 print("ENVIRONMENTAL:")
 print(env_corr)
 corrplot(env_corr$r, type = "full", order = "hclust", 
          tl.col = "black", tl.srt = 45, p.mat = env_corr$P, 
          sig.level = 0.01, insig = "blank")
 
-# OBSERVATION: Aridity, Rainfall, Evapotranspiration are highly correlated, 
-# choose best variable.
+# OBSERVATION: Aridity, Rainfall, Evapotranspiration are highly correlated
 
 # PCA of Aridity, Rainfall, Evapotranspiration -> Humidity Score
 
@@ -60,7 +59,7 @@ env["Humidity Score"] = env_pca$x[,1]
 
 # Geographic/Positional Variables
 
-geo <- df[c("Proximity_to_National_Borders", "Proximity_to_Protected_Areas", 
+geo <- df[c("id", "Proximity_to_National_Borders", "Proximity_to_Protected_Areas", 
             "Proximity_to_Water", "Travel_Times")]
 
 colnames(geo)[colnames(geo) == 'Proximity_to_National_Borders'] <- 'National Borders'
@@ -68,7 +67,7 @@ colnames(geo)[colnames(geo) == 'Proximity_to_Protected_Areas'] <- 'Protected Are
 colnames(geo)[colnames(geo) == 'Proximity_to_Water'] <- 'Water Bodies'
 colnames(geo)[colnames(geo) == 'Travel_Times'] <- 'Nearest City'
 
-geo_corr <- rcorr(as.matrix(geo))
+geo_corr <- rcorr(as.matrix(geo[, !(names(geo) %in% c("id"))]))
 print("GEOGRAPHIC:")
 print(geo_corr)
 corrplot(geo_corr$r, type = "full", order = "hclust", 
@@ -93,12 +92,12 @@ geo["Location Score 2"] = geo_pca$x[, 2]
 
 # Demographic Variables
 
-de <- df[c("All_Population_Count_2010", "All_Population_Density_2010")]
+de <- df[c("id", "All_Population_Count_2010", "All_Population_Density_2010")]
 
 colnames(de)[colnames(de) == 'All_Population_Count_2010'] <- 'Population'
 colnames(de)[colnames(de) == 'All_Population_Density_2010'] <- 'Density'
 
-de_corr <- rcorr(as.matrix(de))
+de_corr <- rcorr(as.matrix(de[, !(names(de) %in% c("id"))]))
 print("DEMOGRAPHIC:")
 print(de_corr)
 corrplot(de_corr$r, type = "full", order = "hclust", 
@@ -113,6 +112,9 @@ print(thresh)
 hist(de$Density,  breaks=30, freq = FALSE)
 abline(v=thresh,col="red")
 
+de["Pop Score"] = as.numeric(de$Density > thresh)
+
+
 # --------------------------
 
 
@@ -121,18 +123,18 @@ abline(v=thresh,col="red")
 night <- df[c("id", "mean")]
 day <- read.csv("../processed/DHS_daytime.csv")
 exp_var <- read.csv("../processed/daytime_explained_variance.csv")
-im <- merge(day, night, by.x="idx", by.y="id")
+im <- merge(night, day, by.x="id", by.y="idx")
 
-im = im[, !(names(df) %in% c("X"))]
+im = im[, !(names(im) %in% c("X"))]
 
-colnames(im)[colnames(im) == 'mean'] <- 'MeanLuminosity'
+colnames(im)[colnames(im) == 'mean'] <- 'Mean Luminosity'
 
 im_corrs = data.frame( coeff=integer(ncol(day)-3), corr=numeric(ncol(day)-3) )
 
 for( i in 0:(ncol(day)-4)) {
     cat(i, "\n")
     im_corrs$coeff[i] = i
-    im_corrs$corr[i] = cor( im[[ paste("X",i, sep="") ]], im$MeanLuminosity)
+    im_corrs$corr[i] = cor( im[[ paste("X",i, sep="") ]], im["Mean Luminosity"])
 }
 
 corr = im_corrs$corr
@@ -142,6 +144,24 @@ plot(coeff, corr)
 plot(PCA~X, data=exp_var)
 
 v <- cumsum(exp_var$PCA)
+print(v/max(v))
 plot(v)
 
+# First 10 for PCA
+
+im_relevant <- im[, c(
+    "id", "X0", "X1", "X2", "X3", "X4", "X5", "X6", "X7", "X8", "X9", "Mean Luminosity", "wealth"
+)]
+
+# --------------------------
+
+# Write Processed data to csv
+
+final <- Reduce(function(x, y) merge(x, y), list(env, geo, de, im_relevant))
+
+final <- final[, !(names(final) %in% c(
+    "Aridity", "Evapotranspiration", "Rainfall", "National Borders", "Protected Areas", "Nearest City"
+))]
+
+write.csv(final, "../processed/all_features.csv")
 
